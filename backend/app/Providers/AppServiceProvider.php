e3751beb9d68c3779582ee0632ed37f60bda09b4
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-05-22 16:35:40 +0900

    Review registering function

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\DataProvider\Database\RegisterReviewDataProvider;
+use App\DataProvider\RegisterReviewProviderInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(RegisterReviewProviderInterface::class, function () {
+            return new RegisterReviewDataProvider();
+        });
     }
 
     /**
