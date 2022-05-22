ad2b48dbc870845f358fd1de0152fb31d9fae122
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-05-22 16:39:05 +0900

    register review endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post(
+    '/review',
+    \App\Http\Controllers\Review\RegisterAction::class
+);
