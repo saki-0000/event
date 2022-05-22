1a21566741acdc0b98619add2117363bd9d0e023
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-05-22 15:42:09 +0900

    make db

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,24 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
-class DatabaseSeeder extends Seeder
+final class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // 外部キーを追加します
+        Schema::table(
+            'reviews',
+            function (Blueprint $table) {
+                $table->foreign('user_id')->references('id')
+                    ->on('users')->onDelete('cascade')->onUpdate('cascade');
+            }
+        );
+        Schema::table(
+            'review_tags',
+            function (Blueprint $table) {
+                $table->foreign('review_id')->references('id')
+                    ->on('reviews')->onDelete('cascade')->onUpdate('cascade');
+                $table->foreign('tag_id')->references('id')
+                    ->on('tags')->onDelete('cascade')->onUpdate('cascade');
+            }
+        );
+        $this->call(
+            [
+                UserSeeder::class
+            ]
+        );
     }
 }
